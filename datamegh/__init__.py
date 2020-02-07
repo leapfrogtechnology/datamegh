9391dac3dc5f4adbd68e86e2d5739e6a7761f357
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-02-07 22:56:06 +0545

    Setup datamegh package

---
+++ datamegh/__init__.py
@@ -0,0 +1,21 @@
+''' Datamegh package. '''
+from os import environ
+from logging import INFO, DEBUG, ERROR, WARNING, CRITICAL
+
+
+name = 'datamegh'
+version = '1.0.0-alpha.1'
+
+log_level_map = {
+    'info': INFO,
+    'debug': DEBUG,
+    'error': ERROR,
+    'warning': WARNING,
+    'critical': CRITICAL
+}
+config = {
+    'logging': {
+        'level': log_level_map.get(environ.get('LOG_LEVEL')) or DEBUG,
+        'format': '%(asctime)s - [ %(levelname)s ] %(name)s - %(message)s'
+    }
+}
