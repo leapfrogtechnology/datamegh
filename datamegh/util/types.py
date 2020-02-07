f11db60f7c1a6722f2ab99f09c59bd0b62f8f00c
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-02-07 23:00:13 +0545

    Add basic utiliy types

---
+++ datamegh/util/types.py
@@ -0,0 +1,29 @@
+''' Type utility functions. '''
+
+
+def is_string(obj):
+    ''' Check if the object is a string. '''
+    try:
+        basestring
+    except NameError:
+        basestring = str
+
+    return isinstance(obj, basestring)
+
+
+def is_iterable(obj):
+    ''' Check if the object is iterable. '''
+    has_iter = hasattr(obj, '__iter__')
+    has_get_item = hasattr(obj, '__getitem__')
+
+    return has_iter or has_get_item
+
+
+def is_dict(obj):
+    ''' Check if the object is a dict. '''
+    return type(obj) == type({})
+
+
+def is_list(value):
+    ''' Check if the given value is a list. '''
+    return isinstance(value, list)
