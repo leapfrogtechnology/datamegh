1690135df977129353d096e5fa9f8f02761e1b97
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-02-09 15:43:30 +0545

    Test MySQL connection strings

--- tests/util/test_db.py
+++ tests/util/test_db.py
@@ -12,14 +12,14 @@ def test_build_connstr_1():
     ''' Test throws error if no client is provided. '''
 
     with pytest.raises(RuntimeError, match='Database `client` must be provided to connect to.'):
-        connstr = build_connstr()
+        build_connstr()
 
 
 def test_build_connstr_1():
     ''' Test throws error if an unsupported client is provided. '''
 
     with pytest.raises(RuntimeError, match='Unsupported database client type "abcdb".'):
-        connstr = build_connstr(client='abcdb')
+        build_connstr(client='abcdb')
 
 
 def test_build_connstr_3():
@@ -45,10 +45,19 @@ def test_build_connstr_3():
 
     assert connstr_pg == 'DRIVER={PostgreSQL Unicode};SERVER=localhost;PORT=5432;DATABASE=test;UID=test;PWD=Test@123'
 
-    # TODO: Add mysql
+    connstr_mysql = build_connstr(
+        client=MYSQL,
+        host='localhost',
+        database='test',
+        username='test',
+        password='Test@123'
+    )
+
+    assert connstr_mysql == 'DRIVER={MySQL ODBC 8.0 Driver};SERVER=localhost;PORT=3306;DATABASE=test;UID=test;PWD=Test@123'
 
 
-def test_build_connstr_3():
+
+def test_build_connstr_4():
     ''' Test it default values could be overwritten for supported clients. '''
 
     connstr_mssql = build_connstr(
@@ -75,7 +84,17 @@ def test_build_connstr_3():
 
     assert connstr_pg == 'DRIVER=SOME_OTHER_DRIVER;SERVER=localhost;PORT=5444;DATABASE=test;UID=test;PWD=Test@123'
 
-    # TODO: Add mysql
+    connstr_mysql = build_connstr(
+        client=MYSQL,
+        port=33066,
+        driver='SOME_OTHER_DRIVER',
+        host='localhost',
+        database='test',
+        username='test',
+        password='Test@123'
+    )
+
+    assert connstr_mysql == 'DRIVER=SOME_OTHER_DRIVER;SERVER=localhost;PORT=33066;DATABASE=test;UID=test;PWD=Test@123'
 
 
 @patch('datamegh.util.db.pyodbc')
