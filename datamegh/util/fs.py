cff5a4115872ea5fc769ce462d3596cfec096ebc
Basanta78 <basmhzn78@gmail.com>
2020-02-17 13:56:24 +0545

    Format remaining code with black

--- datamegh/util/fs.py
+++ datamegh/util/fs.py
@@ -1,28 +1,28 @@
-''' File system utilities. '''
+""" File system utilities. """
 
 import os
 from glob import glob
 
 
 def read(filename):
-    ''' Read contents of file and return it. '''
-    with open(filename, 'r') as f:
+    """ Read contents of file and return it. """
+    with open(filename, "r") as f:
         return f.read()
 
 
 def write(filename, data):
-    ''' Write data to the file. '''
-    with open(filename, 'w') as f:
+    """ Write data to the file. """
+    with open(filename, "w") as f:
         f.write(data)
 
 
 def exists(path):
-    ''' Check if file path exists. '''
+    """ Check if file path exists. """
     return os.path.exists(path)
 
 
 def glob_all(paths):
-    ''' Get a list of all files from the given list of paths. '''
+    """ Get a list of all files from the given list of paths. """
     files = []
 
     for path in paths:
--- setup.py
+++ setup.py
@@ -1,40 +1,32 @@
-''' Package setup. '''
+""" Package setup. """
 
 import setuptools
 
 import datamegh
 
 # Requirements
-requirements = [
-    'pyodbc>=4.0.27,<5',
-    'flatten_json==0.1.7'
-]
+requirements = ["pyodbc>=4.0.27,<5", "flatten_json==0.1.7"]
 
 # Development Requirements
-requirements_dev = [
-    'pytest==4.*',
-    'mock==3.0.5',
-    'black==19.10b0',
-    'pre-commit==2.0.1'
-]
+requirements_dev = ["pytest==4.*", "mock==3.0.5", "black==19.10b0", "pre-commit==2.0.1"]
 
-with open('README.md', 'r') as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
 setuptools.setup(
     name=datamegh.name,
     version=datamegh.version,
-    description='Datamegh - Engineered for the cloud.',
-    author='Kabir Baidhya',
-    author_email='kabirbaidhya@gmail.com',
+    description="Datamegh - Engineered for the cloud.",
+    author="Kabir Baidhya",
+    author_email="kabirbaidhya@gmail.com",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/leapfrogtechnology/datamegh',
+    long_description_content_type="text/markdown",
+    url="https://github.com/leapfrogtechnology/datamegh",
     packages=setuptools.find_packages(),
     install_requires=requirements,
-    extras_require={'dev': requirements_dev},
+    extras_require={"dev": requirements_dev},
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
     ],
 )
