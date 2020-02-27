e6fceeab3bd393955f324549b092eed1c08951bb
Basanta78 <basmhzn78@gmail.com>
2020-02-27 21:07:42 +0545

    Refacor env variable names

--- settings.py
+++ settings.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 # AWS configuration credentials
 AWS_CONFIG = {
-    "aws_access_key_id": os.getenv("ACCESS_KEY"),
-    "aws_secret_access_key": os.getenv("SECRET_KEY"),
+    "aws_access_key_id": os.getenv("AWS_ACCESS_KEY_ID"),
+    "aws_secret_access_key": os.getenv("AWS_SECRET_ACCESS_KEY"),
 }
