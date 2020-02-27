b062c5465750c9fadaec77d08139b09e38c01f6d
Basanta78 <basmhzn78@gmail.com>
2020-02-27 22:51:03 +0545

    Use fuctional approach

--- datamegh/api/storage/s3/s3.py
+++ datamegh/api/storage/s3/s3.py
@@ -4,53 +4,53 @@ from boto3.s3.transfer import S3Transfer
 from boto3.exceptions import S3UploadFailedError
 from botocore.exceptions import ClientError
 
-from datamegh.commons.aws import AWS
+from datamegh.commons.aws import get_aws_client
 from datamegh.util.logging import get_logger
 
+logger = get_logger("storage:s3")
 
-class S3(AWS):
-    def __init__(self):
-        self.logger = get_logger("db")
-        self.client = self.get_client("s3")
 
-    def upload_to_s3(self, file_path, bucket_name, s3_object_name):
-        """
-        Method to upload file to s3 bucket
-        :param file_path: file path
-        :param bucket_name: s3 bucket name
-        :param s3_object_name: name of file to be in s3 bucket
-        """
-        transfer = S3Transfer(self.client)
-        # Upload the file to S3
-        try:
-            transfer.upload_file(file_path, bucket_name, s3_object_name)
-        except FileNotFoundError:
-            self.logger.error("File: {} was not found".format(file_path))
-        except (ClientError, S3UploadFailedError) as se:
-            self.logger.error(se)
-        else:
-            self.logger.info(
-                "File : {} uploaded to {} bucket S3 successfully.".format(
-                    file_path, bucket_name
-                )
+def upload_to_s3(file_path, bucket_name, s3_object_name):
+    """
+    Method to upload file to s3 bucket
+    :param file_path: file path
+    :param bucket_name: s3 bucket name
+    :param s3_object_name: name of file to be in s3 bucket
+    """
+    s3_client = get_aws_client("s3")
+    transfer = S3Transfer(s3_client)
+    # Upload the file to S3
+    try:
+        transfer.upload_file(file_path, bucket_name, s3_object_name)
+    except FileNotFoundError:
+        logger.error("File: {} was not found".format(file_path))
+    except (ClientError, S3UploadFailedError) as se:
+        logger.error(se)
+    else:
+        logger.info(
+            "File : {} uploaded to {} bucket S3 successfully.".format(
+                file_path, bucket_name
             )
+        )
 
-    def download_from_s3(self, file_dir, file_name, bucket_name, s3_object_name):
-        """
-        Method to download file from s3
-        :param file_dir: destination file directory eg:/home/destination/
-        :param file_name: name of the file to be
-        :param bucket_name: s3 bucket name
-        :param s3_object_name: s3 file object name
-        """
-        file_path = file_dir + file_name
-        try:
-            self.client.download_file(bucket_name, s3_object_name, file_path)
-        except ClientError as ce:
-            self.logger.error(ce)
-        else:
-            self.logger.info(
-                "File: {} downloaded successfully in the dir {}.".format(
-                    file_name, file_dir
-                )
+
+def download_from_s3(file_dir, file_name, bucket_name, s3_object_name):
+    """
+    Method to download file from s3
+    :param file_dir: destination file directory eg:/home/destination/
+    :param file_name: name of the file to be
+    :param bucket_name: s3 bucket name
+    :param s3_object_name: s3 file object name
+    """
+    file_path = file_dir + file_name
+    s3_client = get_aws_client("s3")
+    try:
+        s3_client.download_file(bucket_name, s3_object_name, file_path)
+    except ClientError as ce:
+        logger.error(ce)
+    else:
+        logger.info(
+            "File: {} downloaded successfully in the dir {}.".format(
+                file_name, file_dir
             )
+        )
--- datamegh/commons/aws.py
+++ datamegh/commons/aws.py
@@ -4,20 +4,12 @@ import boto3
 from settings import AWS_CONFIG
 
 
-class AWS:
-    def __get_session(self):
-        """
-        Private method to generate AWS session
-        :return: session
-        """
-        session = boto3.Session(**AWS_CONFIG)
-        return session
-
-    def get_client(self, client_name):
-        """
-        Method to get AWS client object
-        :param client_name: name of the AWS client
-        :return: AWS client
-        """
-        client = self.__get_session().client(client_name)
-        return client
+def get_aws_client(client_name):
+    """
+    Method to get AWS client object
+    :param client_name: name of the AWS client
+    :return: AWS client
+    """
+    session = boto3.Session(**AWS_CONFIG)
+    client = session.client(client_name)
+    return client
