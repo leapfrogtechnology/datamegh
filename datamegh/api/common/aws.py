3ede0998b8c2e9d2a19526b5a0611170e3fc10d5
Basanta78 <basmhzn78@gmail.com>
2020-03-02 18:02:03 +0545

    Add type check paramter in methods and api refactor

--- datamegh/api/common/aws.py
+++ datamegh/api/common/aws.py
@@ -1,10 +1,11 @@
 """ AWS base utility. """
 import boto3
+from botocore import client
 
 from settings import AWS_CONFIG
 
 
-def get_aws_client(client_name):
+def get_aws_client(client_name: str):
     """
     Method to get AWS client object
     :param client_name: name of the AWS client
--- datamegh/api/storage/s3/s3.py
+++ datamegh/api/storage/s3/s3.py
@@ -4,13 +4,13 @@ from boto3.s3.transfer import S3Transfer
 from boto3.exceptions import S3UploadFailedError
 from botocore.exceptions import ClientError
 
-from datamegh.commons.aws import get_aws_client
+from datamegh.api.common.aws import get_aws_client
 from datamegh.util.logging import get_logger
 
 logger = get_logger("storage:s3")
 
 
-def upload(file_path, bucket_name, s3_object_name):
+def upload(file_path: str, bucket_name: str, s3_object_name: str) -> None:
     """
     Method to upload file to s3 bucket
     :param file_path: file path
@@ -34,7 +34,9 @@ def upload(file_path, bucket_name, s3_object_name):
         )
 
 
-def download(file_dir, file_name, bucket_name, s3_object_name):
+def download(
+    file_dir: str, file_name: str, bucket_name: str, s3_object_name: str
+) -> None:
     """
     Method to download file from s3
     :param file_dir: destination file directory eg:/home/destination/
