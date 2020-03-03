f354eb87060ff27ad0b97ff066f99559d517547d
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-03-03 21:52:08 +0545

    Move settings to datamegh.config

--- settings.py
+++ settings.py
@@ -1,7 +1,3 @@
 import os
 
-# AWS configuration credentials
-AWS_CONFIG = {
-    "aws_access_key_id": os.getenv("AWS_ACCESS_KEY_ID"),
-    "aws_secret_access_key": os.getenv("AWS_SECRET_ACCESS_KEY"),
-}
+
