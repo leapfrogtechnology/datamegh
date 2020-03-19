bffeb2b8035e758a5d4ce44ed52e5d588726167d
Bimochan Shrestha <bmochan@gmail.com>
2020-03-19 16:05:36 +0545

    http-utils: Add basic http verbs for api connections

--- datamegh/api/http.py
+++ datamegh/api/http.py
@@ -1,1 +1,38 @@
-""" HTTP Connection API. """
+""" Basic HTTP verbs for your API connection. """
+
+import requests
+
+
+def get(url, params=None, **kwargs):
+    response = requests.get(url, params, **kwargs)
+    return response
+
+
+def options(url, **kwargs):
+    response = requests.options(url, **kwargs)
+    return response
+
+
+def head(url, **kwargs):
+    response = requests.head(url, **kwargs)
+    return response
+
+
+def post(url, data=None, json=None, **kwargs):
+    response = requests.post(url, data, json, **kwargs)
+    return response
+
+
+def put(url, data=None, **kwargs):
+    response = requests.put(url, data, **kwargs)
+    return response
+
+
+def patch(url, data=None, **kwargs):
+    response = requests.patch(url, data, **kwargs)
+    return response
+
+
+def delete(url, **kwargs):
+    response = requests.delete(url, **kwargs)
+    return response
