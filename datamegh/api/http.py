60ca55488ec42a09c04881b36921026ccb90f121
Bimochan Shrestha <bmochan@gmail.com>
2020-03-19 18:43:30 +0545

    http-utils: Add func doc and rewrite into one line

--- datamegh/api/http.py
+++ datamegh/api/http.py
@@ -4,35 +4,90 @@ import requests
 
 
 def get(url, params=None, **kwargs):
-    response = requests.get(url, params, **kwargs)
-    return response
+    """Sends a GET request.
+
+    :param url: URL for the new :class:`Request` object.
+    :param params: (optional) Dictionary, list of tuples or bytes to send
+        in the query string for the :class:`Request`.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    """
+    return requests.get(url, params, **kwargs)
 
 
 def options(url, **kwargs):
-    response = requests.options(url, **kwargs)
-    return response
+    """Sends an OPTIONS request.
+
+    :param url: URL for the new :class:`Request` object.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    """
+    return requests.options(url, **kwargs)
 
 
 def head(url, **kwargs):
-    response = requests.head(url, **kwargs)
-    return response
+    """Sends a HEAD request.
+
+    :param url: URL for the new :class:`Request` object.
+    :param \*\*kwargs: Optional arguments that ``request`` takes. If
+        `allow_redirects` is not provided, it will be set to `False` (as
+        opposed to the default :meth:`request` behavior).
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    """
+    return requests.head(url, **kwargs)
 
 
 def post(url, data=None, json=None, **kwargs):
-    response = requests.post(url, data, json, **kwargs)
-    return response
+    """Sends a POST request.
+
+    :param url: URL for the new :class:`Request` object.
+    :param data: (optional) Dictionary, list of tuples, bytes, or file-like
+        object to send in the body of the :class:`Request`.
+    :param json: (optional) json data to send in the body of the :class:`Request`.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    """
+    return requests.post(url, data, json, **kwargs)
 
 
 def put(url, data=None, **kwargs):
-    response = requests.put(url, data, **kwargs)
-    return response
+    """Sends a PUT request.
+
+    :param url: URL for the new :class:`Request` object.
+    :param data: (optional) Dictionary, list of tuples, bytes, or file-like
+        object to send in the body of the :class:`Request`.
+    :param json: (optional) json data to send in the body of the :class:`Request`.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    """
+    return requests.put(url, data, **kwargs)
 
 
 def patch(url, data=None, **kwargs):
-    response = requests.patch(url, data, **kwargs)
-    return response
+    """Sends a PATCH request.
+
+    :param url: URL for the new :class:`Request` object.
+    :param data: (optional) Dictionary, list of tuples, bytes, or file-like
+        object to send in the body of the :class:`Request`.
+    :param json: (optional) json data to send in the body of the :class:`Request`.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    """
+    return requests.patch(url, data, **kwargs)
 
 
 def delete(url, **kwargs):
-    response = requests.delete(url, **kwargs)
-    return response
+    """Sends a DELETE request.
+
+    :param url: URL for the new :class:`Request` object.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    """
+    return requests.delete(url, **kwargs)
